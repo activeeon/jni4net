7937348c2a82549a2d3e2ea4b34115438277a6f2
nebilbenmabrouk <nebil.mabrouk@activeeon.com>
2017-10-04 19:09:11 +0200

    fix jni4net so it compiles

--- tools/keys/gennetkey.cmd
+++ tools/keys/gennetkey.cmd
@@ -1,2 +1,1 @@
-if not exist "%~dp0/jni4net.snk" "%ProgramFiles%\Microsoft SDKs\Windows\v7.0A\Bin\sn.exe" -k "%~dp0/jni4net.snk"
-if not exist "%~dp0/jni4net.snk" "%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\sn.exe" -k "%~dp0/jni4net.snk"
+if not exist "%~dp0/jni4net.snk" "%C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.7 Tools\sn.exe" -k "%~dp0/jni4net.snk"
